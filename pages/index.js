a4c2a364654260005a645848610113e1eb4f6730
ycruzb <ycruzb@gmail.com>
2021-02-03 18:45:49 -0500

    changing the chart to bar

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,16 @@ import Link from "next/link";
 import LoadingSpinner from "../components/loadingSpinner";
 import useSWR from "swr";
 
-import { ResponsiveContainer, PieChart, Pie, Legend } from "recharts";
+import {
+  BarChart,
+  Bar,
+  Cell,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+} from "recharts";
 
 const fetcher = async (url) => {
   const res = await fetch(url);
@@ -22,15 +31,52 @@ const Home = () => {
   const router = useRouter();
   const [session, loading] = useSession();
 
-  const dataPie = [
-    { name: "Group A", value: 400 },
-    { name: "Group B", value: 300 },
-    { name: "Group C", value: 300 },
-    { name: "Group D", value: 200 },
-  ];
-  const COLORS = ["#0088FE", "#00C49F", "#FFBB28", "#FF8042"];
-
   const { data, errorData } = useSWR(`/api/transactions`, fetcher);
+
+  const dataPie = [
+    {
+      name: "Page A",
+      uv: 4000,
+      pv: 2400,
+      amt: 2400,
+    },
+    {
+      name: "Page B",
+      uv: 3000,
+      pv: 1398,
+      amt: 2210,
+    },
+    {
+      name: "Page C",
+      uv: 2000,
+      pv: 9800,
+      amt: 2290,
+    },
+    {
+      name: "Page D",
+      uv: 2780,
+      pv: 3908,
+      amt: 2000,
+    },
+    {
+      name: "Page E",
+      uv: 1890,
+      pv: 4800,
+      amt: 2181,
+    },
+    {
+      name: "Page F",
+      uv: 2390,
+      pv: 3800,
+      amt: 2500,
+    },
+    {
+      name: "Page G",
+      uv: 3490,
+      pv: 4300,
+      amt: 2100,
+    },
+  ];
 
   React.useEffect(() => {
     //console.log(session, loading);
@@ -98,16 +144,25 @@ const Home = () => {
                 <h3 className="text-sm font-bold mb-4 text-center md:text-left">
                   Sold by Store
                 </h3>
-                <div className="w-full h-16">
+                <div className="w-full h-48">
                   <ResponsiveContainer>
-                    <PieChart>
-                      <Pie
-                        dataKey="value"
-                        data={dataPie}
-                        fill="#8884d8"
-                        label
-                      />
-                    </PieChart>
+                    <BarChart
+                      data={dataPie}
+                      margin={{
+                        top: 5,
+                        right: 30,
+                        left: 20,
+                        bottom: 5,
+                      }}
+                    >
+                      <CartesianGrid strokeDasharray="3 3" />
+                      <XAxis dataKey="name" />
+                      <YAxis />
+                      <Tooltip />
+                      <Legend />
+                      <Bar dataKey="pv" fill="#8884d8" />
+                      <Bar dataKey="uv" fill="#82ca9d" />
+                    </BarChart>
                   </ResponsiveContainer>
                 </div>
               </div>
