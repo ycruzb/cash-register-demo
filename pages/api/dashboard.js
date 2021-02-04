88cb1f7824c75a19b4a809a79258bfc60efdfcd9
ycruzb <ycruzb@gmail.com>
2021-02-04 18:27:48 -0500

    enabling dashboard api endpoint

--- pages/api/dashboard.js
+++ pages/api/dashboard.js
@@ -1,147 +1,137 @@
 const connectToDatabase = require("../../utils/mongoDB");
-import { getSession } from "next-auth/client";
 
 module.exports = async (req, res) => {
-  const session = await getSession({ req });
+  const db = await connectToDatabase(process.env.DATABASE_URL);
 
-  if (session) {
-    const db = await connectToDatabase(process.env.DATABASE_URL);
+  const collectionTransactions = await db.collection("transactions");
 
-    const collectionTransactions = await db.collection("transactions");
+  const collectionStores = await db.collection("stores");
 
-    const collectionStores = await db.collection("stores");
+  const transactionsCount = await collectionTransactions.countDocuments({});
 
-    const transactionsCount = await collectionTransactions.countDocuments({});
+  const totalPurchase = await collectionTransactions
+    .aggregate([
+      {
+        $match: {
+          transaction_type: "Purchase",
+        },
+      },
+      {
+        $group: { _id: "$transaction_type", total: { $sum: "$price" } },
+      },
+    ])
+    .toArray();
 
-    const totalPurchase = await collectionTransactions
-      .aggregate([
-        {
-          $match: {
-            transaction_type: "Purchase",
-          },
+  const totalSold = await collectionTransactions
+    .aggregate([
+      {
+        $match: {
+          transaction_type: "Sold",
         },
-        {
-          $group: { _id: "$transaction_type", total: { $sum: "$price" } },
-        },
-      ])
-      .toArray();
+      },
+      {
+        $group: { _id: "$transaction_type", total: { $sum: "$price" } },
+      },
+    ])
+    .toArray();
 
-    const totalSold = await collectionTransactions
-      .aggregate([
-        {
-          $match: {
-            transaction_type: "Sold",
-          },
+  const purchaseByStore = await collectionStores
+    .aggregate([
+      {
+        $lookup: {
+          from: "transactions",
+          localField: "_id",
+          foreignField: "store_id",
+          as: "transaction",
         },
-        {
-          $group: { _id: "$transaction_type", total: { $sum: "$price" } },
+      },
+      {
+        $unwind: "$transaction",
+      },
+      {
+        $project: {
+          _id: "$_id",
+          name: "$name",
+          transaction_type: "$transaction.transaction_type",
+          price: "$transaction.price",
         },
-      ])
-      .toArray();
+      },
+      {
+        $match: {
+          transaction_type: "Purchase",
+        },
+      },
+      {
+        $group: {
+          _id: "$name",
+          purchase: { $sum: "$price" },
+        },
+      },
+    ])
+    .toArray();
 
-    const purchaseByStore = await collectionStores
-      .aggregate([
-        {
-          $lookup: {
-            from: "transactions",
-            localField: "_id",
-            foreignField: "store_id",
-            as: "transaction",
-          },
+  const soldByStore = await collectionStores
+    .aggregate([
+      {
+        $lookup: {
+          from: "transactions",
+          localField: "_id",
+          foreignField: "store_id",
+          as: "transaction",
         },
-        {
-          $unwind: "$transaction",
+      },
+      {
+        $unwind: "$transaction",
+      },
+      {
+        $project: {
+          _id: "$_id",
+          name: "$name",
+          transaction_type: "$transaction.transaction_type",
+          price: "$transaction.price",
         },
-        {
-          $project: {
-            _id: "$_id",
-            name: "$name",
-            transaction_type: "$transaction.transaction_type",
-            price: "$transaction.price",
-          },
+      },
+      {
+        $match: {
+          transaction_type: "Sold",
         },
-        {
-          $match: {
-            transaction_type: "Purchase",
-          },
+      },
+      {
+        $group: {
+          _id: "$name",
+          sold: { $sum: "$price" },
         },
-        {
-          $group: {
-            _id: "$name",
-            purchase: { $sum: "$price" },
-          },
-        },
-      ])
-      .toArray();
+      },
+    ])
+    .toArray();
 
-    const soldByStore = await collectionStores
-      .aggregate([
-        {
-          $lookup: {
-            from: "transactions",
-            localField: "_id",
-            foreignField: "store_id",
-            as: "transaction",
-          },
-        },
-        {
-          $unwind: "$transaction",
-        },
-        {
-          $project: {
-            _id: "$_id",
-            name: "$name",
-            transaction_type: "$transaction.transaction_type",
-            price: "$transaction.price",
-          },
-        },
-        {
-          $match: {
-            transaction_type: "Sold",
-          },
-        },
-        {
-          $group: {
-            _id: "$name",
-            sold: { $sum: "$price" },
-          },
-        },
-      ])
-      .toArray();
+  var stores = await collectionStores.find({}).toArray();
 
-    var stores = await collectionStores.find({}).toArray();
+  storesData = stores.map((store) => {
+    let purchase = purchaseByStore
+      .filter((item) => item["_id"] === store.name)
+      .reduce(
+        (acc, item) =>
+          item["_id"] === store.name ? acc + item.purchase : acc + 0,
+        0
+      );
 
-    storesData = stores.map((store) => {
-      let purchase = purchaseByStore
-        .filter((item) => item["_id"] === store.name)
-        .reduce(
-          (acc, item) =>
-            item["_id"] === store.name ? acc + item.purchase : acc + 0,
-          0
-        );
+    let sold = soldByStore
+      .filter((item) => item["_id"] === store.name)
+      .reduce(
+        (acc, item) => (item["_id"] === store.name ? acc + item.sold : acc + 0),
+        0
+      );
 
-      let sold = soldByStore
-        .filter((item) => item["_id"] === store.name)
-        .reduce(
-          (acc, item) =>
-            item["_id"] === store.name ? acc + item.sold : acc + 0,
-          0
-        );
+    return { name: store.name, purchase: purchase, sold: sold };
+  });
 
-      return { name: store.name, purchase: purchase, sold: sold };
-    });
-
-    res.status(200).json({
-      totalPurchase,
-      totalSold,
-      purchaseByStore,
-      soldByStore,
-      transactionsCount,
-      storesData,
-    });
-  } else {
-    res.send({
-      error: "You must be sign in to view the protected content on this page.",
-    });
-  }
+  res.status(200).json({
+    totalPurchase,
+    totalSold,
+    purchaseByStore,
+    soldByStore,
+    transactionsCount,
+    storesData,
+  });
 };
